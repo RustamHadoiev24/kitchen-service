46a165cb04549fab7b51d7ba04396e16304af8a6
RustamHadoiev24 <gadoeva155@gmail.com>
2026-06-07 19:36:09 +0300

    feat: add superuser and restore local kitchen data

--- kitchen/migrations/0002_create_superuser.py
+++ kitchen/migrations/0002_create_superuser.py
@@ -1,7 +1,8 @@
 from django.db import migrations
+import os
 
 
-def create_superuser_cook(apps, schema_editor):
+def create_superuser_and_load_data(apps, schema_editor):
     from django.contrib.auth import get_user_model
     Cook = get_user_model()
 
@@ -18,6 +19,22 @@ def create_superuser_cook(apps, schema_editor):
         )
         user.set_password("user12345")
         user.save()
+        print("=== Суперюзера 'user' успішно створено! ===")
+
+    from django.core.management import call_command
+    from django.conf import settings
+
+    fixture_path = os.path.join(settings.BASE_DIR, 'datadump.json')
+
+    if os.path.exists(fixture_path):
+        try:
+            print(f"=== Знайдено файл даних за шляхом: {fixture_path}. Починаємо імпорт... ===")
+            call_command('loaddata', fixture_path)
+            print("=== Усі локальні дані успішно перенесені на Render! ===")
+        except Exception as e:
+            print(f"⚠️ Помилка під час виконання loaddata: {e}")
+    else:
+        print(f"⚠️ Файл {fixture_path} не знайдено в корені проєкту. Пропускаємо імпорт даних.")
 
 
 class Migration(migrations.Migration):
@@ -26,5 +43,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_superuser_cook),
+        migrations.RunPython(create_superuser_and_load_data),
     ]
